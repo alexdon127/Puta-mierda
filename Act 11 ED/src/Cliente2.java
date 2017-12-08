258d6c67cf4b5e6924385507d128b9d8735888b6
Alejandro Doña <alexdon127@hotmail.com>
2017-12-08 19:53:52 +0100

    Borrada la rama secundaria

---
+++ Act 11 ED/src/Cliente2.java
@@ -0,0 +1,62 @@
+
+/**
+@author: Alejandro Doña
+@version: 01/12/2017
+*/
+public class Cliente2 extends Object {
+	
+	public int codigo;
+	public String nombre;
+	public String apellido;
+	public String email;
+	public long telefono;
+	public boolean nuevo;
+	public double cifraVentas;
+	
+	public Cliente2() {
+		//TODO Auto-generated constructor stub
+	}
+	
+	/** @param codigo Codigo del cliente
+	 * @param nombre Nombre del cliente
+	 * @param email Email del cliente  
+	*/
+	public Cliente2(int codigo, String nombre, String email){
+		super(); //Object()
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.email = email;
+	}
+	/** @param codigo Codigo del cliente
+	 * @param nombre Nombre del cliente
+	 * @param email Email del cliente  
+	 * @param telefono Telefono del cliente
+	 * @param nuevo Comprobación de si es un nuevo cliente o no
+	 * @param cifraVentas Cifras de las ventas del cliente
+	*/
+	
+	public Cliente2(int codigo, String nombre, String email, long telefono, boolean nuevo, double cifraVentas) {
+		super(); 
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.email = email;	
+		this.telefono = telefono;
+		this.nuevo = nuevo;
+		this.cifraVentas = cifraVentas;
+	}
+	
+	//acciones -> metodos
+	public void mostrarDatos(){
+		System.out.println("Codigo: " + codigo + "Nombre: " + nombre + "...");
+	}
+	/**
+	 * @return El numero de cifras de las ventas del cliente selecionado
+	 */
+	public double verCifraVentas(){
+		return cifraVentas;
+	}
+	
+	public void cambiarTelefono(long nuevoTelefono){
+		telefono = nuevoTelefono;
+	}
+}
